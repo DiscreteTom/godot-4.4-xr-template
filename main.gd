3be434a33b237957e55a99ca82a1a7cdc86b6353
DiscreteTom <discrete_tom@outlook.com>
2025-07-15 21:35:03 +0800

    feat: VST (AR mode) is working, add switch_to_ar and switch_to_vr function

--- main.gd
+++ main.gd
@@ -1,9 +1,41 @@
 extends Node3D
 
-var xr_interface: XRInterface
+@onready var xr_interface: XRInterface = XRServer.find_interface("OpenXR")
+@onready var viewport : Viewport = get_viewport()
+@onready var environment : Environment = $WorldEnvironment.environment
+
+func switch_to_ar() -> bool:
+	if xr_interface:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		print(modes)
+		if XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND
+			viewport.transparent_bg = true
+		elif XRInterface.XR_ENV_BLEND_MODE_ADDITIVE in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_ADDITIVE
+			viewport.transparent_bg = false
+	else:
+		return false
+
+	environment.background_mode = Environment.BG_COLOR
+	environment.background_color = Color(0.0, 0.0, 0.0, 0.0)
+	environment.ambient_light_source = Environment.AMBIENT_SOURCE_COLOR
+	return true
+
+func switch_to_vr() -> bool:
+	if xr_interface:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		if XRInterface.XR_ENV_BLEND_MODE_OPAQUE in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_OPAQUE
+		else:
+			return false
+
+	viewport.transparent_bg = false
+	environment.background_mode = Environment.BG_SKY
+	environment.ambient_light_source = Environment.AMBIENT_SOURCE_BG
+	return true
 
 func _ready():
-	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialized successfully")
 
@@ -11,6 +43,8 @@ func _ready():
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
 		# Change our main viewport to output to the HMD
-		get_viewport().use_xr = true
+		viewport.use_xr = true
+		
+		# switch_to_ar()
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
