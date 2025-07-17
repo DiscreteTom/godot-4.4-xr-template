689d5152adbbe2f037477332844e144eb554fc50
DiscreteTom <discrete_tom@outlook.com>
2025-07-17 21:28:05 +0800

    fix: check for null when checking input

--- scripts/controller_io.gd
+++ scripts/controller_io.gd
@@ -1,9 +1,9 @@
 extends XRController3D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# Use action name from OpenXR Action Map
 	# See https://docs.godotengine.org/en/stable/tutorials/xr/xr_action_map.html
 	var grip = get_input('grip')
-	if grip >= 0.99:
+	if grip != null && grip >= 0.99:
 		print('grip', grip)
 		trigger_haptic_pulse("haptic", 60, 0.5, 0.2, 0)
